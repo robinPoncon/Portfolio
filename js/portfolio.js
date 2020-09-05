f8b6ca2cf91e9a797fad283a85dd22f6fc424983
Robin Ponçon <poncon.robin@gmail.com>
2020-09-05 11:58:11 +0200

    finition partie compétences et début formation

---
+++ js/portfolio.js
@@ -0,0 +1,19 @@
+$(".langages").hover(function () {
+    remplissage("#html", "85%", 2000);
+    remplissage("#css", "80%", 2000);
+    remplissage("#bootstrap", "40%", 2000);
+    remplissage("#javascript", "60%", 2000);
+    remplissage("#jquery", "70%", 2000);
+    remplissage("#php", "60%", 2000);
+    remplissage("#mysql", "40%", 2000);
+    remplissage("#symfony", "50%", 2000);
+    remplissage("#twig", "70%", 2000);
+    remplissage("#wordpress", "40%", 2000);
+});
+
+function remplissage(cible, width, time)
+{
+    $(cible).animate({
+        width: width
+    }, time);
+}
