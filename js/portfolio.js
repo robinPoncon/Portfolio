36258025c8836e7e8edad49fe21f6e924468e82a
Robin Ponçon <poncon.robin@gmail.com>
2020-09-06 16:32:37 +0200

    finition formation et début portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -11,9 +11,34 @@ $(".langages").hover(function () {
     remplissage("#wordpress", "40%", 2000);
 });
 
+$(".blocFormation1").hover(function(){
+    $(".barreHorizon1").animate({
+        width: "119px"
+    }, 2000);
+    $(".barreHorizon2").animate({
+        width: "119px"
+    }, 2000);
+});
+$(".blocFormation2").hover(function(){
+    $(".barreHorizon3").animate({
+        width: "119px"
+    }, 2000);
+    $(".barreHorizon4").animate({
+        width: "119px"
+    }, 2000);
+});
+
 function remplissage(cible, width, time)
 {
     $(cible).animate({
         width: width
     }, time);
 }
+
+$(".cache3").on("mouseenter", function(){
+    $(".bandeau3").css("display", "block");
+});
+
+$(".cache3").on("mouseleave", function(){
+    $(".bandeau3").css("display", "none");
+});
